35e53d22eaf6553718afa74ee092e35ead4698e6
Yassinelaz1 <yassinelazhari524@gmail.com>
2025-04-09 19:47:58 +0100

    update

--- utils/redis.mjs
+++ utils/redis.mjs
@@ -1,53 +1,41 @@
-// utils/redis.mjs
 import { createClient } from 'redis';
 
+class RedisClient {
+  constructor() {
+    this.client = createClient();
+    this.client.on('error', (err) => console.error('Redis Client Error:', err));
+    this.client.connect(); // Needed in redis v4+
+  }
 
-class RedisClient{
-    constructor(){
-        this.client = createClient();
-        this.isCconnected = false;
+  isAlive() {
+    return this.client.isReady;
+  }
 
-        this.client.on('error', (err) => {
-            console.error('Redis Client Error', err);
-        });
-        this.client.on('connect', () => {
-            this.isCconnected = true;
-        });
-        this.client.connect().catch((err) => {
-            console.error('Redis connection failed:', err);
-          });
-
+  async get(key) {
+    try {
+      return await this.client.get(key);
+    } catch (err) {
+      console.error('Redis GET Error:', err);
+      return null;
     }
-    isAlive(){
-        return this.isCconnected;
+  }
+
+  async set(key, value, duration) {
+    try {
+      await this.client.set(key, value, { EX: duration });
+    } catch (err) {
+      console.error('Redis SET Error:', err);
     }
-    async get(key) {
-        try {
-          return await this.client.get(key);
-        } catch (err) {
-          console.error('Redis GET Error:', err);
-          return null;
-        }
-      }
+  }
 
-    async set(key, value, duration) {
-        try {
-          await this.client.set(key, value, {
-            EX: duration,
-          });
-        } catch (err) {
-          console.error('Redis SET Error:', err);
-        }
-      }
-
-      async del(key){
-        try {
-            await this.client.del(key);
-        } catch (err) {
-            console.error('Redis DEL Error:', err);
-        }
-      }
-
+  async del(key) {
+    try {
+      await this.client.del(key);
+    } catch (err) {
+      console.error('Redis DEL Error:', err);
+    }
+  }
 }
+
 const redisClient = new RedisClient();
 export default redisClient;
