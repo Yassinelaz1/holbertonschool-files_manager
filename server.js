bee326fafa81fa2f872cdf0be79a57ca294dc93d
Yassinelaz1 <yassinelazhari524@gmail.com>
2025-04-10 15:29:16 +0100

    update

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-import router from './routes/index';
-
-const express = require('express');
+import express from 'express';
+import routes from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-router(app);
+app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
